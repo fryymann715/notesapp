be3e3dd3323802b8f681fcafcabd1557a223be65
fryymann715 <ideans715@gmail.com>
2016-01-02 14:44:38 -0800

    12/29/15

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ import noteclasses
 class MainHandler(lessonhandlers.Handler):
     def get(self):
         dgoods = noteclasses.Dgoods()
-        ##self.render("new-base.html", dgoods=dgoods)
         self.render("base.html", dgoods=dgoods)
 
 
